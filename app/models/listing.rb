617bdedffff02f5c52f24241e6624c3b275dfb1f
Nes14 <Hernandeznes@gmail.com>
2015-11-16 10:06:33 -0600

    add figaro

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,8 @@
 class Listing < ActiveRecord::Base
 
-  has_attached_file :image, styles: { medium: "160x>", thumb: "100x100>" }, default_url: "slice1.png"
+  has_attached_file :image, styles: { medium: "160x>", thumb: "100x100>" }, default_url: "slice1.png",
+  	:storage => :dropbox,
+  	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    
 end
