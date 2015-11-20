4c09056fa4a4e3f13237914f1c7121da413cca8c
Nes14 <Hernandeznes@gmail.com>
2015-11-20 12:28:51 -0600

    add extra field to listings

---
+++ db/migrate/20151117180337_add_user_idto_listings.rb
@@ -0,0 +1,4 @@
+class AddUserIdtoListings < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117172248) do
+ActiveRecord::Schema.define(version: 20151117180337) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151117172248) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id_"
   end
 
   create_table "models", force: :cascade do |t|
